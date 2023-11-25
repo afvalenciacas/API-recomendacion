2fcfd86b06b2f2c1759f11f7b9d4802a26a93fa2
dianavillalba <93057437+dianavillalba@users.noreply.github.com>
2023-11-25 12:29:08 -0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, jsonify, g
 from flask_cors import CORS
 import pandas as pd
 import numpy as np
-import json
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.decomposition import TruncatedSVD
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
@@ -45,10 +44,11 @@ def custom_recommendation_model(df, generos_usuario, seleccion_usuario, n_compon
     # Convertir a matriz dispersa
     atributos_latentes_sparse = csr_matrix(atributos_latentes)
     
+    print(f'Recomendaciones Modelo antes similitud del coseno')
     # Calcular similitud del coseno
     similitud = cosine_similarity(atributos_latentes_sparse) if n_components < min(atributos.shape) else cosine_similarity(atributos)
     indices_recomendaciones = similitud.sum(axis=0).argsort()[::-1]
-    
+
     print(f'Recomendaciones Modelo despues similitud coseno')
     recomendaciones = subset_df.iloc[indices_recomendaciones].head(10)
     print(f'Recomendaciones Modelo: {recomendaciones.head(10)}')
